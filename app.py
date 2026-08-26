828ce75d2481b091e724fa79d2a50461edc2f2a2
Robert Penate <61477326+Excludedfeline1@users.noreply.github.com>
2026-08-26 11:13:42 -0400

    Revert "bugfixinf duplictation glitch"
    This reverts commit 4386ddb4479d3a2d7d136b1fdb89a7bc1e983507.

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from connect import run_query
 zoomLink=run_query("SELECT * FROM zoom")[0]["zlink"]
 
 pages = [
-    st.Page("app_pages/home.py", title="Home Page"),
-    st.Page("app_pages/admin.py", title="Admin Page")
+    st.Page("pages/home.py", title="Home Page"),
+    st.Page("pages/admin.py", title="Admin Page")
 ]
 
 pg = st.navigation(pages)
